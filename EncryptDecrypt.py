dd6cc0c7d27d04b92d27c0ca567a330c87097a55
lgeorgeson17 <118539597+lgeorgeson17@users.noreply.github.com>
2023-03-09 17:39:35 +0000

    Encryption Algorithm (Not complete)

---
+++ EncryptDecrypt.py
@@ -0,0 +1,26 @@
+import Asciichar as ac
+def toInt(word):
+    integer = []
+    for i in range(len(word)):
+        integer.append(ord(word[i]))
+    return integer
+def encrypt(word):
+    encrypted, encryptedlist, encryptedstr = [],[],""
+    integer = toInt(word)
+    for i in range(len(integer)):
+        encrypted.append(integer[i] << 2)
+        encryptedstr += chr(encrypted[i])
+    return encryptedstr
+
+def decrypt(word):
+    integer, decryptedlist, decrypted = [],[],""
+    for i in range(len(word)):
+        integer.append(ord(word[i]))
+        decryptedlist.append(integer[i] >> 2)
+        decrypted += chr(decryptedlist[i])
+    return decrypted
+
+
+encryptedword = encrypt("Hello World")
+print(encryptedword)
+print(decrypt(encryptedword))
