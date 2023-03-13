a252bd60db3b62d0979686e1cf4258ec128bb51d
lgeorgeson17 <118539597+lgeorgeson17@users.noreply.github.com>
2023-03-13 14:45:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
-from kivy.app import runTouchApp
 from kivy.app import App
-import sqlite3
+import sqlite3, AddLogin as AL, EncryptDecrypt as ED
 class PasswordManager(App):
     def build(self):
         layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
         # Make sure the height is such that there is something to scroll.
         layout.bind(minimum_height=layout.setter('height'))
-        for i in range(100):
-            btn = Button(text=str(i), size_hint_y=None, height=40)
+        #Collect Data For Passwords
+        conn = sqlite3.connect("Logins.db")
+        c = conn.cursor()
+        c.execute("SELECT *,oid FROM LoginPassword")
+        records = c.fetchall()
+        c.close()
+        print_records = ""
+        for record in records:
+            print_records = "Website: " + str(record[0]) + "\nUsername:" + str(record[1])
+            btn = Button(text=str(print_records), size_hint_y=None, height=40)
             layout.add_widget(btn)
         root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
         root.add_widget(layout)
